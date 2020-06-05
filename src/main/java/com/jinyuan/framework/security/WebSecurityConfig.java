942c7a5377876bcc56cbd83cfd96b91ae40efac2
beautiful-ppi <1301860547@qq.com>
2020-06-06 00:22:13 +0800

    git remote add origin https://github.com/beautiful-ppi/school-trade.git git pull -u origin master

---
+++ src/main/java/com/jinyuan/framework/security/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.jinyuan.framework.security;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author Aaron
+ * @description :
+ * @Date 2020/6/5
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //配置允许跨域
+        //csrf()  跨站请求伪造  解决POST 403问题
+        http.csrf().disable().cors();
+    }
+}
---
+++ src/main/java/com/jinyuan/framework/security/service/PermissionService.java
@@ -0,0 +1,13 @@
+package com.jinyuan.framework.security.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Aaron
+ * @description : 自定义权限认证
+ * @Date 2020/6/5
+ */
+@Service("Permission")
+public class PermissionService {
+
+}
